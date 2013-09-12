4aad0ed2b4951699f0728cb0e732a433d6a2d72f
marvinick <marviluw@gmail.com>
2013-09-12 13:37:43 -0500

    create category

--- postit/app/controllers/categories_controller.rb
+++ postit/app/controllers/categories_controller.rb
@@ -1,5 +1,16 @@
-class CategoriesController < ApplicationController 
-  def new 
-    @category = Category.new 
-  end 
- end 
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new (params.require(:category).permit(:name))
+
+    if @category.save
+      flash[:notice] = "Your category was created"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+ end
