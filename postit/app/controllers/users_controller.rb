64e93666e4baca48f6a324672f1fb03b24466b3a
marvinick <marviluw@gmail.com>
2013-09-30 08:35:48 -0500

    user update action

--- postit/app/controllers/users_controller.rb
+++ postit/app/controllers/users_controller.rb
@@ -18,14 +18,17 @@
     @user = User.find_by(slug: params[:id])
   end
 
-  # def update
-   # if @user.update(user_params)
-    #  flash[:notice] = "This user was updated"
-     # redirect_to root_path
-    #else
-     # render :edit
-    #end
-  # end
+  def update
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "You are updated"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 
   private
 
